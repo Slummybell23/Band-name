69e52f56df5cddb085e37b76fdc2019f668bd087
Slummybell23 <76507206+Slummybell23@users.noreply.github.com>
2021-01-06 18:22:29 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+city = input("What city were you born in? ")
+pet = input("What name was your first pet? ")
+
+print("Your band name could look like: " + city + " " + pet)
